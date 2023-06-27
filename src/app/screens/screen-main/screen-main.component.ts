f915b562c986234cb3ddf66618bcd2899720f8d1
Byroks <byroks@gmail.com>
2023-06-27 13:38:58 +0200

    base working frontend

--- src/app/screens/screen-main/screen-main.component.ts
+++ src/app/screens/screen-main/screen-main.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { Boons } from 'src/app/boons';
 import { QueryService } from 'src/app/services/query-service.service';
 
@@ -7,11 +13,16 @@ import { QueryService } from 'src/app/services/query-service.service';
   templateUrl: './screen-main.component.html',
   styleUrls: ['./screen-main.component.scss'],
 })
-export class ScreenMainComponent implements OnInit {
+export class ScreenMainComponent implements OnInit, AfterViewInit {
+  @ViewChild('csvDownload') csvDownload?: ElementRef;
   public boonWeights?: Boons;
-  private file?: File;
+  private uploadfile?: File;
   private send = false;
   public fileName = '';
+  private fileContent: any;
+  public jsonUrl: any;
+  public csvUrl: any;
+  public toggle = false;
 
   constructor(private queryService: QueryService) {}
 
@@ -21,17 +32,44 @@ export class ScreenMainComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    this.queryService.getDefaultWeights().subscribe((data) => {
+      this.boonWeights = data;
+    });
+  }
+
   onFileSelected(event: any) {
-    this.file = event.target.files[0];
-    if (this.file) {
-      this.fileName = this.file.name;
+    this.uploadfile = event.target.files[0];
+    if (this.uploadfile) {
+      this.fileName = this.uploadfile.name;
+      let fileReader = new FileReader();
+      fileReader.readAsText(this.uploadfile);
+      fileReader.onload = (e) => {
+        this.fileContent = fileReader.result;
+      };
+    }
+  }
+
+  downloadFile(data: any, count: boolean) {
+    if (count) {
+      const blob = new File([data], 'boons-list.csv');
+      this.csvUrl = window.URL.createObjectURL(blob);
+    } else {
+      const blob = new File([JSON.stringify(data)], this.fileName);
+      this.jsonUrl = window.URL.createObjectURL(blob);
     }
   }
 
   onSend() {
-    if (this.file) {
-      const upload$ = this.queryService.postUploadFile(this.file);
-      upload$.subscribe();
+    if (this.uploadfile) {
+      const upload$ = this.queryService.postUploadFile({
+        file: JSON.parse(this.fileContent),
+        weights: this.boonWeights,
+      });
+      upload$.subscribe((x) => {
+        this.downloadFile(x.csv, true);
+        this.downloadFile(x.json, false);
+      });
       this.send = true;
     }
   }
