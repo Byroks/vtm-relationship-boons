55eff5d20270636d9b6324147ba088e0258ee100
Byroks <byroks@gmail.com>
2023-09-09 22:11:15 +0200

    change node version

--- src/app/services/query-service.service.ts
+++ src/app/services/query-service.service.ts
@@ -9,7 +9,7 @@ import { Boons } from 'src/app/boons';
 export class QueryService {
   constructor(private http: HttpClient) {}
 
-  url = 'https://boonsbynight:3000';
+  url = 'https://95.111.232.136:3000';
 
   getDefaultWeights(): Observable<any> {
     return this.http.get<Boons>(`${this.url}/api/default-weights`);
